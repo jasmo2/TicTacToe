f9cc5e3780f9fa906f9443aaa61930cc86aed7b8
jasmo2 <jasmo2.com@gmail.com>
2015-02-24 07:54:08 -0500

    add base images

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var vertical_1: UILabel!
+    @IBOutlet var vertical_2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var radians = CGFloat(-M_PI / 2)
+        vertical_1.transform = CGAffineTransformMakeRotation(radians)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
