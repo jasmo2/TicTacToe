2d3a649e527215708197ec2282156484336b8a6c
jasmo2 <jasmo2.com@gmail.com>
2015-03-02 09:12:30 -0500

    basic game logica accomplish

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,8 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    var actualPlay:Int = 0
+    var ticTacImg = UIImage()
+    // -1 == Cross; 0 == Empty; 1 = Nought
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    @IBOutlet var button1_1: UIButton!
+    
+    @IBAction func gameLogic(sender: AnyObject) {
+        actualPlay += 1
+        
+        if gameState[sender.tag] == 0 { 
+            if actualPlay % 2 == 0{
+                ticTacImg.self = UIImage(named: "nought.png")!
+                gameState[sender.tag] = 1
+            }else{
+                ticTacImg.self = UIImage(named: "cross.png")!
+                gameState[sender.tag] = -1
+            }
+            
+            sender.setImage(ticTacImg, forState: .Normal)
+        }
+    }
+    let winnigCombinations:[[Int]] = [[1,5,9],[1,2,3],[1,6,7],[2,5,8],[3,5,7],[3,4,9],[4,5,6],[7,8,9]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
